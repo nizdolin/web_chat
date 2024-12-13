7bc0cabee8d7d7dd6a5adbdfd4206b0ac08cca7a
nizdolin <nizdolin@gmail.com>
2024-12-13 11:58:45 +0300

    Configure project

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -2,4 +2,6 @@ from fastapi import FastAPI
 
 
 def create_app():
-    return FastAPI(title="Web Chat", docs_url="/api/docs", description="Simple Websocket chat based on DDD and Kafka")
+    return FastAPI(
+        title="Web Chat", docs_url="/api/docs", description="A simple web chat based on Websockets, DDD and Kafka"
+    )
